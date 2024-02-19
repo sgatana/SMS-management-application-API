c796e49ee53f930a5c598004d3a0d1a70b1b878c
Stephen Gatana <31792736+sgatana@users.noreply.github.com>
2024-02-19 09:40:48 +0300

    add log

--- start/index.js
+++ start/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('koajs-cors')
 
 const app = new Koa()
+console.log(db)
 app.context.db = db
 app.use(cors())
 app.use(bodyParser())
